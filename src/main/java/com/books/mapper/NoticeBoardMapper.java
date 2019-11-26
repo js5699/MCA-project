f3abf3020abfd878c8dec93417e16bfd3c4fce7f
js5699 <gundam_zeta@naver.com>
2019-11-26 16:56:36 +0900

    이름변경

--- src/main/java/com/books/mapper/NoticeBoardMapper.java
+++ src/main/java/com/books/mapper/NoticeBoardMapper.java
@@ -2,11 +2,24 @@ package com.books.mapper;
 
 import java.util.List;
 
+import com.books.domain.Criteria;
 import com.books.domain.NoticeBoardVO;
 
 public interface NoticeBoardMapper {
 
 	public List<NoticeBoardVO> getList();
 	
-	public List<NoticeBoardVO> getListWithPaging();
+	public List<NoticeBoardVO> getListWithPaging(Criteria cri);
+	
+	public void insert(NoticeBoardVO NBVO);
+	
+	public void insertSelectKey(NoticeBoardVO NBVO);
+	
+	public NoticeBoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(NoticeBoardVO NBVO);
+	
+	public int getTotalCount(Criteria cri);
 }
